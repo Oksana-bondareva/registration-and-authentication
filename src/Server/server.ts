55c22b0312ca4be846374d8bb55873def35a5d4e
Oksana Bondareva <oksanchik252@gmail.com>
2024-11-22 22:47:02 +0300

    fix: correct port

--- src/Server/server.ts
+++ src/Server/server.ts
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = new Koa();
 const router = new Router();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3306;
 
 app.use(bodyParser());
 app.use(cors());
